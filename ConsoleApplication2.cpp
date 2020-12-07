6a4cf59ea42ce761baa77bb80be0164f71c1f278
GodBoyDad <75253734+GodBoyDad@users.noreply.github.com>
2020-12-07 16:46:05 +0600

    Add files via upload

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,20 +1,19 @@
-﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿
 
 #include <iostream>
 using namespace std;
 int main()
 {
     setlocale(LC_ALL, "ru");
-    int Var, Var2, Var3;
-    Var2 = 5;
+    int Var, Var2;
+    Var2 = 5; //Var наше число. Var 2 количество товаров на складе.
     cout << "Введите количество единиц товара: " << endl;
     cin >> Var;
 
-    if (Var2 <= Var)
-        cout << "Покупка успешно совершенна! " << endl;
+    if (Var2 >= Var)
+        cout << "Покупка успешно совершенна! " << endl; //число равно или меньше чем на складе
     else if(Var2 < Var)
-        cout << "Транзакция отменена." << endl;
+        cout << "Транзакция отменена." << endl; //отменяет транзкакцию
 
      else if (Var < 0)
         cout << "Ваше число меньше нуля" << endl;
